afae22050f81b3bb4b0de676f5724fd7025fb1d3
Jan Kytka <kytka.honza@gmail.com>
2018-04-16 21:47:41 +0200

    Added auto update

--- dist/tampermonkey-userscript.js
+++ dist/tampermonkey-userscript.js
@@ -7,6 +7,7 @@
 // @require      https://raw.githubusercontent.com/Muph0/facebook-pgp/master/facebook-pgp.js
 // @require      https://raw.githubusercontent.com/openpgpjs/openpgpjs/master/dist/openpgp.js
 // @match        https://www.facebook.com/messages/*/*
+// @downloadURL  https://raw.githubusercontent.com/Muph0/facebook-pgp/master/dist/tampermonkey-userscript.js
 // @grant GM_setValue
 // @grant GM_getValue
 // @grant GM_listValues
